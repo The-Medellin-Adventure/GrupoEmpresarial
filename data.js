830e31c00fbe3c45f1f4813400a4fa823d4ffb3d
The-Medellin-Adventure <dcaame@gmail.com>
2026-05-18 15:06:30 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,70 @@
+const toursData = {
+
+  guatape:[
+    {
+      titulo:"Guatapé Full Day",
+      imagen:"tours/guatape.jpg",
+      descripcion:`
+        <h3>Experiencia</h3>
+        <p>
+          Descubre Guatapé, la Piedra del Peñol y la represa
+          en una experiencia llena de color y cultura.
+        </p>
+
+        <h3>Incluye</h3>
+
+        <p>
+          ✔ Transporte turístico<br>
+          ✔ Guía bilingüe<br>
+          ✔ Almuerzo típico<br>
+          ✔ Paseo en barco
+        </p>
+      `
+    }
+  ],
+
+  ciudad:[
+    {
+      titulo:"City Tour Medellín",
+      imagen:"tours/ciudad.jpg",
+      descripcion:`
+        <h3>Experiencia</h3>
+
+        <p>
+          Conoce la transformación social y cultural de Medellín.
+        </p>
+
+        <h3>Incluye</h3>
+
+        <p>
+          ✔ Metro y Metrocable<br>
+          ✔ Plaza Botero<br>
+          ✔ Guía bilingüe
+        </p>
+      `
+    }
+  ],
+
+  aventura:[
+    {
+      titulo:"Tour Aventura",
+      imagen:"tours/aventura.jpg",
+      descripcion:`
+        <h3>Experiencia</h3>
+
+        <p>
+          Naturaleza, adrenalina y experiencias inolvidables.
+        </p>
+
+        <h3>Incluye</h3>
+
+        <p>
+          ✔ Actividades extremas<br>
+          ✔ Transporte<br>
+          ✔ Guía
+        </p>
+      `
+    }
+  ]
+
+};
